adf869211952a47bc34e36adf3a20234305f22a1
Rahul <rahul.dabhi117@gmail.com>
2017-11-22 00:27:20 +0530

    last commit with class file add

---
+++ MYClass.swift
@@ -0,0 +1,13 @@
+//
+//  MYClass.swift
+//  MYFirstframwork
+//
+//  Created by Rahul Dabhi on 30/10/17.
+//  Copyright © 2017 Rahul Dabhi. All rights reserved.
+//
+
+import Foundation
+public func testHelloPrint(strPrint :String) {
+    print(strPrint)
+    
+}
